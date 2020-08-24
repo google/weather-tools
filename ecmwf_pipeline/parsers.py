be72bf50ba5558a5cd1bc60cfaa685ee795349c7
Alex Rosengarten <alxrsngrtn@gmail.com>
2020-08-23 17:25:46 -0700

    Data Partitioning via Configuration
    This commit introduces a new strategy for dividing
    work in the beam pipeline. Instead of iterating
    over time, the divion of data can be determined
    by the user via the `partition_keys` field of the
    configuration file. (More information can be
    found in docs/Configuration.md).
    
    This also introduces a structure for the config
    files, complete with documentation and helpful
    error messages to guide the user.
    
    Squashed commit of the following:
    
    commit 5c11676bb4a07f4af6c8d9fe74a6658fed636c18
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 17:21:10 2020 -0700
    
        Tested config parsing e2e. Fixed issues.
        - DEFAULT is not allowed anymore.
    
    commit 01407b6953a6597c2333f75f82ae3c956489702f
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 17:00:45 2020 -0700
    
        Wrote "good-enough" documentation.
    
    commit 5e2376776790be758ac4a7e7c3b00bc631e18d28
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 16:45:17 2020 -0700
    
        Another layer of documentation.
    
    commit 8e34957250879a5cd4aa73df6fa6eb0ddcada44f
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 16:29:38 2020 -0700
    
        Simplified CLI API, given new direction of config file.
    
    commit 7b4527f6e9932f1f8ab61d3fb44876c8255fd352
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 16:26:57 2020 -0700
    
        Started config file documentation.
    
    commit 16765ce3ffc193718b8bcdeb9656054656f8053d
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 16:10:58 2020 -0700
    
        Partitioning configuration by `partition_keys`
    
        This commit adds a `prepare_partition` function, which uses the `paritition_keys` from the `parameters` section to divide work for downloading downstream.
    
    commit 25ba20e1c079f661a9ca4afffd99ff7c0cefd079
    Author: Alex Rosengarten <alxrsngrtn@gmail.com>
    Date:   Sun Aug 23 15:38:42 2020 -0700
    
        Created config processor
        - Informs user how to structure the config file with descriptive error messages
        - Fix: *.cfg files will interpret multi-line blocks as lists
        - Added example config file (example_config.cfg).

--- ecmwf_pipeline/parsers.py
+++ ecmwf_pipeline/parsers.py
@@ -4,6 +4,7 @@ import datetime
 import io
 import json
 import typing as t
+import textwrap
 
 
 def date(candidate: str) -> datetime.date:
@@ -28,8 +29,77 @@ def parse_config(file: io.StringIO) -> t.Dict:
     try:
         config = configparser.ConfigParser()
         config.read_file(file)
-        return {s: dict(config.items(s)) for s in config.sections()}
+        return {s: _parse_lists(dict(config.items(s))) for s in config.sections()}
     except configparser.ParsingError:
         pass
 
     return {}
+
+
+def _splitlines(block: str) -> t.List[str]:
+    """Converts a multi-line block into a list of strings."""
+    return [line.strip() for line in block.strip().splitlines()]
+
+
+def _parse_lists(config: t.Dict) -> t.Dict:
+    """Parses multiline blocks in *.cfg files as lists."""
+    for key, val in config.items():
+        if '\n' in val:
+            config[key] = _splitlines(val)
+    return config
+
+
+def process_config(file: io.StringIO) -> t.Dict:
+    """Read the config file and prompt the user if it is improperly structured."""
+    config = parse_config(file)
+
+    def require(condition: bool, message: str) -> None:
+        """A assert-like helper that wraps text and throws a `ValueError`."""
+        if not condition:
+            raise ValueError(textwrap.dedent(message))
+
+    require(bool(config), "Unable to parse configuration file.")
+    require('parameters' in config,
+            """
+            'parameters' section required in configuration file.
+            
+            The 'parameters' section specifies the 'dataset', 'target_template', and
+            'partition_key' for the API client.
+            
+            Please consult the documentation for more information.""")
+
+    params = config.get('parameters', {})
+    require('dataset' in params,
+            """
+            'parameters' section requires a 'dataset' key.
+            
+            The 'dataset' value is used to choose which data product to download from the
+            API client.
+            
+            Please consult the client documentation for more information about what value 
+            to specify.""")
+    require('target_template' in params,
+            """
+            'parameters' section requires a 'target_template' key.
+            
+            The 'target_template' is used to format the name of the output files. It
+            accepts Python 3.5+ string format symbols (e.g. '{}'). The number of symbols
+            should match the length of the 'partition_keys', as the 'partition_keys' args
+            are used to create the templates.""")
+
+    partition_keys = params.get('partition_keys', list())
+    if isinstance(partition_keys, str):
+        partition_keys = [partition_keys.strip()]
+
+    selection = config.get('selection', dict())
+    require(all((key in selection for key in partition_keys)),
+            """
+            All 'partition_keys' must appear in the 'selection' section.
+            
+            'partition_keys' specify how to split data for workers. Please consult 
+            documentation for more information.""")
+
+    # Ensure consistent lookup.
+    config['parameters']['partition_keys'] = partition_keys
+
+    return config
