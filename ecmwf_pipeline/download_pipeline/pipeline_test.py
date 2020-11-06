4d1fea35370a9f90f3b9e873d093faf33965c423
Shirish Jamthe <sjamthe@google.com>
2020-11-06 18:40:19 +0000

    add ability to skip already downloaded files on job retry b/171403238

--- ecmwf_pipeline/download_pipeline/pipeline_test.py
+++ ecmwf_pipeline/download_pipeline/pipeline_test.py
@@ -4,6 +4,7 @@ from unittest.mock import patch, ANY
 from .clients import CdsClient
 from .pipeline import fetch_data
 from .pipeline import prepare_partition
+from .pipeline import skip_partition
 
 
 class PreparePartitionTest(unittest.TestCase):
@@ -12,6 +13,7 @@ class PreparePartitionTest(unittest.TestCase):
         config = {
             'parameters': {
                 'partition_keys': ['year'],
+                'target_template': 'download-{}.nc',
             },
             'selection': {
                 'features': ['pressure', 'temperature', 'wind_speed_U', 'wind_speed_V'],
@@ -31,6 +33,7 @@ class PreparePartitionTest(unittest.TestCase):
         config = {
             'parameters': {
                 'partition_keys': ['year', 'month'],
+                'target_template': 'download-{}-{}.nc',
             },
             'selection': {
                 'features': ['pressure', 'temperature', 'wind_speed_U', 'wind_speed_V'],
@@ -65,12 +68,66 @@ class PreparePartitionTest(unittest.TestCase):
             }
         }
 
-        _ = fetch_data(config, client=CdsClient(config))
+        fetch_data(config, client=CdsClient(config))
 
         mock_retrieve.assert_called_with(
             'reanalysis-era5-pressure-levels',
             config['selection'],
             ANY)
+
+    def test_skip_partition_missing_force_download(self):
+        config = {
+            'parameters': {
+                'partition_keys': ['year', 'month'],
+                'target_template': 'download-{}-{}.nc',
+            },
+            'selection': {
+                'features': ['pressure', 'temperature', 'wind_speed_U', 'wind_speed_V'],
+                'month': [str(i) for i in range(1, 13)],
+                'year': [str(i) for i in range(2015, 2021)]
+            }
+        }
+
+        actual = skip_partition(config)
+
+        self.assertEqual(actual, False)
+
+    def test_skip_partition_force_download_true(self):
+        config = {
+            'parameters': {
+                'partition_keys': ['year', 'month'],
+                'target_template': 'download-{}-{}.nc',
+                'force_download': True
+            },
+            'selection': {
+                'features': ['pressure', 'temperature', 'wind_speed_U', 'wind_speed_V'],
+                'month': [str(i) for i in range(1, 13)],
+                'year': [str(i) for i in range(2015, 2021)]
+            }
+        }
+
+        actual = skip_partition(config)
+
+        self.assertEqual(actual, False)
+
+    @patch('apache_beam.io.gcp.gcsio.GcsIO.exists')
+    def test_skip_partition_force_download_false(self, mock_exists):
+        config = {
+            'parameters': {
+                'partition_keys': ['year', 'month'],
+                'target_template': 'download-{}-{}.nc',
+                'force_download': False
+            },
+            'selection': {
+                'features': ['pressure'],
+                'month': ['12'],
+                'year': ['02']
+            }
+        }
+
+        actual = skip_partition(config)
+
+        self.assertEqual(actual, True)
 
 
 if __name__ == '__main__':
