35ad0f3e854e3c8aa500474a077beed9fdd406d0
David Lowell <dlowell@google.com>
2020-12-09 02:20:25 +0000

    Write netcdf contents to BigQuery without bloating memory

--- netcdf_loader/loader_pipeline/netcdf_loader_test.py
+++ netcdf_loader/loader_pipeline/netcdf_loader_test.py
@@ -1,19 +1,28 @@
 import unittest
 import numpy as np
 import pandas as pd
+import xarray as xr
 from google.cloud.bigquery import SchemaField
 
-from netcdf_loader.loader_pipeline.netcdf_loader import dataframe_to_table_schema
+from netcdf_loader.loader_pipeline.netcdf_loader import dataset_to_table_schema
 
 class NetcdfLoaderTest(unittest.TestCase):
 
     def test_schema_generation(self):
-        d = {'a': [pd.Timestamp(0)], 'b': [np.float32(1.0)],
-             'c': [np.float64(2.0)], 'd': [3.0]}
-        df = pd.DataFrame(data=d)
-        schema = dataframe_to_table_schema(df)
-        expected_schema = [SchemaField('index', 'INT64', 'REQUIRED', None, (), None),
-                           SchemaField('a', 'TIMESTAMP', 'REQUIRED', None, (), None),
+        d = {
+            "coords": {"a": {"dims": ("a",), "data": [pd.Timestamp(0)], "attrs": {}}},
+            "attrs": {},
+            "dims": "a",
+            "data_vars": {
+                "b": {"dims": ("a",), "data": [np.float32(1.0)]},
+                "c": {"dims": ("a",), "data": [np.float64(2.0)]},
+                "d": {"dims": ("a",), "data": [3.0]},
+            }
+        }
+
+        ds = xr.Dataset.from_dict(d)
+        schema = dataset_to_table_schema(ds)
+        expected_schema = [SchemaField('a', 'TIMESTAMP', 'REQUIRED', None, (), None),
                            SchemaField('b', 'FLOAT64', 'REQUIRED', None, (), None),
                            SchemaField('c', 'FLOAT64', 'REQUIRED', None, (), None),
                            SchemaField('d', 'FLOAT64', 'REQUIRED', None, (), None),
