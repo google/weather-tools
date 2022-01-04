a1f4e2efd090162e15da2913a108e4ad9bee8f88
Cillian Fennell <cfennell.stflannans@gmail.com>
2022-01-04 19:52:26 +0000

    Read the docs config (#61)
    * read the docs & dependencies
    
    * test build on commit
    
    * Revert "test build on commit"
    
    This reverts commit 0fd2116be71a10c17ffec0f2bd2c3d9ec4ce3cc2.
    
    * use alabaster theme

--- docs/conf.py
+++ docs/conf.py
@@ -74,3 +74,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# https://stackoverflow.com/a/66295922/809705
+autodoc_typehints = "description"
