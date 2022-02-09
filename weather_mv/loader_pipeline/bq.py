eb95533dec135d9637bc95afe09ce38b2661cf4a
Alex Merose <alxr@google.com>
2022-02-08 17:42:18 -0800

    Refactored weather-mv to work with pluggable Data Sinks. (#101)
    * Refactored weather-mv to work with pluggable Data Sinks.
    
    - Created abstraction for Data Sinks (where weather data can be moved to)
    - Pulled out existing pipeline into a BigQuery sink
    - Added test stubs
    - Added `dataclasses` as a project dependency for older python versions
    
    TODO:
    - e2e test of the weather mover
    - implement tests
    - document
    
    * Small change to test to deal with private files (like `.DS_Store`).
    
    * Fix lint issues.
    
    * ++Documentation in BQ sink.
    
    * Added tests of pipeline CLI.
    
    * Fix! We shouldn't iterate through the example uris!
    
    * Added tests for sinks.py
    
    * Increment version.
    
    * Dataclass accepts kwargs, dropping irrelevant arguments.
    
    * Extracted non-BQ code from BQ code.

---
+++ weather_mv/loader_pipeline/bq.py
@@ -0,0 +1,203 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import dataclasses
+import datetime
+import logging
+import typing as t
+
+import apache_beam as beam
+import numpy as np
+import pandas as pd
+import xarray as xr
+from apache_beam.io import WriteToBigQuery, BigQueryDisposition
+from google.cloud import bigquery
+from xarray.core.utils import ensure_us_time_resolution
+
+from .sinks import ToDataSink, open_dataset
+from .util import to_json_serializable_type, _only_target_vars, get_coordinates
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+DEFAULT_IMPORT_TIME = datetime.datetime.utcfromtimestamp(0).replace(tzinfo=datetime.timezone.utc).isoformat()
+DATA_IMPORT_TIME_COLUMN = 'data_import_time'
+DATA_URI_COLUMN = 'data_uri'
+DATA_FIRST_STEP = 'data_first_step'
+
+
+@dataclasses.dataclass
+class ToBigQuery(ToDataSink):
+    """Load weather data into Google BigQuery.
+
+    A sink that loads de-normalized weather data into BigQuery. First, this sink will
+    create a BigQuery table from user input (either from `variables` or by inferring the
+    schema). Next, it will convert the weather data into rows and then write each row to
+    the BigQuery table.
+
+    During a batch job, this transform will use the BigQueryWriter's file processing
+    step, which requires that a `temp_location` is passed into the main CLI. This
+    transform will perform streaming writes to BigQuery during a streaming Beam job. See
+    `these docs`_ for more.
+
+    Attributes:
+        example_uri: URI to a weather data file, used to infer the BigQuery schema.
+        output_table: The destination for where data should be written in BigQuery
+        infer_schema: If true, this sink will attempt to read in an example data file
+          read all its variables, and generate a BigQuery schema.
+        import_time: The time when data was imported. This is used as a simple way to
+          version data — variables can be distinguished based on import time. If None,
+          the system will recompute the current time upon row extraction for each file.
+
+    .. _these docs: https://beam.apache.org/documentation/io/built-in/google-bigquery/#setting-the-insertion-method
+    """
+    example_uri: str
+    output_table: str
+    infer_schema: bool
+    import_time: t.Optional[datetime.datetime]
+
+    def __post_init__(self):
+        """Initializes Sink by creating a BigQuery table based on user input."""
+        # Define table from user input
+        if self.variables and not self.infer_schema:
+            logger.info('Creating schema from input variables.')
+            table_schema = to_table_schema(
+                [('latitude', 'FLOAT64'), ('longitude', 'FLOAT64'), ('time', 'TIMESTAMP')] +
+                [(var, 'FLOAT64') for var in self.variables]
+            )
+        else:
+            logger.info('Inferring schema from data.')
+            with open_dataset(self.example_uri, self.xarray_open_dataset_kwargs) as open_ds:
+                ds: xr.Dataset = _only_target_vars(open_ds, self.variables)
+                table_schema = dataset_to_table_schema(ds)
+
+        # Create the table in BigQuery
+        try:
+            table = bigquery.Table(self.output_table, schema=table_schema)
+            self.table = bigquery.Client().create_table(table, exists_ok=True)
+        except Exception as e:
+            logger.error(f'Unable to create table in BigQuery: {e}')
+            raise
+
+    def expand(self, paths):
+        """Extract rows of variables from data paths into a BigQuery table."""
+        (
+                paths
+                | 'ExtractRows' >> beam.FlatMap(
+                    extract_rows,
+                    variables=self.variables,
+                    area=self.area,
+                    import_time=self.import_time,
+                    open_dataset_kwargs=self.xarray_open_dataset_kwargs)
+                | 'WriteToBigQuery' >> WriteToBigQuery(
+                    project=self.table.project,
+                    dataset=self.table.dataset_id,
+                    table=self.table.table_id,
+                    write_disposition=BigQueryDisposition.WRITE_APPEND,
+                    create_disposition=BigQueryDisposition.CREATE_NEVER)
+
+        )
+
+
+def map_dtype_to_sql_type(var_type: np.dtype) -> str:
+    """Maps a np.dtype to a suitable BigQuery column type."""
+    if var_type in {np.dtype('float64'), np.dtype('float32'), np.dtype('timedelta64[ns]')}:
+        return 'FLOAT64'
+    elif var_type in {np.dtype('<M8[ns]')}:
+        return 'TIMESTAMP'
+    elif var_type in {np.dtype('int8'), np.dtype('int16'), np.dtype('int32'), np.dtype('int64')}:
+        return 'INT64'
+    raise ValueError(f"Unknown mapping from '{var_type}' to SQL type")
+
+
+def dataset_to_table_schema(ds: xr.Dataset) -> t.List[bigquery.SchemaField]:
+    """Returns a BigQuery table schema able to store the data in 'ds'."""
+    # Get the columns and data types for all variables in the dataframe
+    columns = [
+        (str(col), map_dtype_to_sql_type(ds.variables[col].dtype))
+        for col in ds.variables.keys() if ds.variables[col].size != 0
+    ]
+
+    return to_table_schema(columns)
+
+
+def to_table_schema(columns: t.List[t.Tuple[str, str]]) -> t.List[bigquery.SchemaField]:
+    # Fields are all Nullable because data may have NANs. We treat these as null.
+    fields = [
+        bigquery.SchemaField(column, var_type, mode='NULLABLE')
+        for column, var_type in columns
+    ]
+
+    # Add an extra columns for recording import metadata.
+    fields.append(bigquery.SchemaField(DATA_IMPORT_TIME_COLUMN, 'TIMESTAMP', mode='NULLABLE'))
+    fields.append(bigquery.SchemaField(DATA_URI_COLUMN, 'STRING', mode='NULLABLE'))
+    fields.append(bigquery.SchemaField(DATA_FIRST_STEP, 'TIMESTAMP', mode='NULLABLE'))
+
+    return fields
+
+
+def extract_rows(uri: str, *,
+                 variables: t.Optional[t.List[str]] = None,
+                 area: t.Optional[t.List[int]] = None,
+                 import_time: t.Optional[str] = DEFAULT_IMPORT_TIME,
+                 open_dataset_kwargs: t.Optional[t.Dict] = None) -> t.Iterator[t.Dict]:
+    """Reads named netcdf then yields each of its rows as a dict mapping column names to values."""
+    logger.info(f'Extracting rows as dicts: {uri!r}.')
+
+    # re-calculate import time for streaming extractions.
+    if not import_time:
+        import_time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+
+    with open_dataset(uri, open_dataset_kwargs) as ds:
+        data_ds: xr.Dataset = _only_target_vars(ds, variables)
+
+        if area:
+            n, w, s, e = area
+            data_ds = data_ds.sel(latitude=slice(n, s), longitude=slice(w, e))
+            logger.info(f'Data filtered by area, size: {data_ds.nbytes}')
+
+        first_ts_raw = data_ds.time[0].values if data_ds.time.size > 1 else data_ds.time.values
+        first_time_step = to_json_serializable_type(first_ts_raw)
+
+        def to_row(it: t.Dict) -> t.Dict:
+            """Produce a single row, or a dictionary of all variables at a point."""
+
+            # Use those index values to select a Dataset containing one row of data.
+            row_ds = data_ds.loc[it]
+
+            # Create a Name-Value map for data columns. Result looks like:
+            # {'d': -2.0187, 'cc': 0.007812, 'z': 50049.8, 'rr': None}
+            temp_row = row_ds.to_pandas().apply(to_json_serializable_type)
+            # Pandas coerces floating type None values back to NaNs, need to do an explicit replace after.
+            row = temp_row.astype(object).where(pd.notnull(temp_row), None).to_dict()
+
+            # Add indexed coordinates.
+            row.update(it)
+            # Add un-indexed coordinates.
+            for c in row_ds.coords:
+                if c not in it and (not variables or c in variables):
+                    row[c] = to_json_serializable_type(ensure_us_time_resolution(row_ds[c].values))
+
+            # Add import metadata.
+            row[DATA_IMPORT_TIME_COLUMN] = import_time
+            row[DATA_URI_COLUMN] = uri
+            row[DATA_FIRST_STEP] = first_time_step
+
+            # 'row' ends up looking like:
+            # {'latitude': 88.0, 'longitude': 2.0, 'time': '2015-01-01 06:00:00', 'd': -2.0187, 'cc': 0.007812,
+            #  'z': 50049.8, 'data_import_time': '2020-12-05 00:12:02.424573 UTC', ...}
+            beam.metrics.Metrics.counter('Success', 'ExtractRows').inc()
+            return row
+
+        yield from map(to_row, get_coordinates(data_ds, uri))
---
+++ weather_mv/loader_pipeline/bq_test.py
@@ -0,0 +1,360 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import datetime
+import logging
+import typing as t
+import unittest
+from contextlib import contextmanager
+from time import perf_counter
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+from google.cloud.bigquery import SchemaField
+
+from .bq import dataset_to_table_schema, extract_rows
+from .sinks_test import TestDataBase, _handle_missing_grib_be
+from .util import _only_target_vars
+
+logger = logging.getLogger(__name__)
+
+
+class SchemaCreationTests(TestDataBase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.test_dataset = {
+            "coords": {"a": {"dims": ("a",), "data": [pd.Timestamp(0)], "attrs": {}}},
+            "attrs": {},
+            "dims": "a",
+            "data_vars": {
+                "b": {"dims": ("a",), "data": [np.float32(1.0)]},
+                "c": {"dims": ("a",), "data": [np.float64(2.0)]},
+                "d": {"dims": ("a",), "data": [3.0]},
+            }
+        }
+
+    def test_schema_generation(self):
+        ds = xr.Dataset.from_dict(self.test_dataset)
+        schema = dataset_to_table_schema(ds)
+        expected_schema = [
+            SchemaField('a', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('b', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('c', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('d', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('data_import_time', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('data_uri', 'STRING', 'NULLABLE', None, (), None),
+            SchemaField('data_first_step', 'TIMESTAMP', 'NULLABLE', None, (), None),
+        ]
+        self.assertListEqual(schema, expected_schema)
+
+    def test_schema_generation__with_target_columns(self):
+        target_variables = ['c', 'd']
+        ds = _only_target_vars(xr.Dataset.from_dict(self.test_dataset), target_variables)
+        schema = dataset_to_table_schema(ds)
+        expected_schema = [
+            SchemaField('a', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('c', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('d', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('data_import_time', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('data_uri', 'STRING', 'NULLABLE', None, (), None),
+            SchemaField('data_first_step', 'TIMESTAMP', 'NULLABLE', None, (), None),
+        ]
+        self.assertListEqual(schema, expected_schema)
+
+    def test_schema_generation__no_targets_specified(self):
+        target_variables = []  # intentionally empty
+        ds = _only_target_vars(xr.Dataset.from_dict(self.test_dataset), target_variables)
+        schema = dataset_to_table_schema(ds)
+        expected_schema = [
+            SchemaField('a', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('b', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('c', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('d', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('data_import_time', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('data_uri', 'STRING', 'NULLABLE', None, (), None),
+            SchemaField('data_first_step', 'TIMESTAMP', 'NULLABLE', None, (), None),
+        ]
+        self.assertListEqual(schema, expected_schema)
+
+    def test_schema_generation__missing_target(self):
+        with self.assertRaisesRegex(AssertionError, 'Target variable must be in original dataset.'):
+            target_variables = ['a', 'foobar', 'd']
+            _only_target_vars(xr.Dataset.from_dict(self.test_dataset), target_variables)
+
+    @_handle_missing_grib_be
+    def test_schema_generation__non_index_coords(self):
+        test_single_var = xr.open_dataset(
+            f'{self.test_data_folder}/test_data_grib_single_timestep',
+            engine='cfgrib'
+        )
+        schema = dataset_to_table_schema(test_single_var)
+        expected_schema = [
+            SchemaField('number', 'INT64', 'NULLABLE', None, (), None),
+            SchemaField('time', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('step', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('surface', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('latitude', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('longitude', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('valid_time', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('z', 'FLOAT64', 'NULLABLE', None, (), None),
+            SchemaField('data_import_time', 'TIMESTAMP', 'NULLABLE', None, (), None),
+            SchemaField('data_uri', 'STRING', 'NULLABLE', None, (), None),
+            SchemaField('data_first_step', 'TIMESTAMP', 'NULLABLE', None, (), None),
+
+        ]
+        self.assertListEqual(schema, expected_schema)
+
+
+class ExtractRowsTestBase(TestDataBase):
+
+    def assertRowsEqual(self, actual: t.Dict, expected: t.Dict):
+        self.assertEqual(expected.keys(), actual.keys())
+        for key in expected.keys():
+            self.assertAlmostEqual(actual[key], expected[key], places=4)
+            self.assertNotIsInstance(actual[key], np.dtype)
+            self.assertNotIsInstance(actual[key], np.float64)
+            self.assertNotIsInstance(actual[key], np.float32)
+
+
+class ExtractRowsTest(ExtractRowsTestBase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.test_data_path = f'{self.test_data_folder}/test_data_20180101.nc'
+
+    def test_extract_rows(self):
+        actual = next(extract_rows(self.test_data_path))
+        expected = {
+            'd2m': 242.3035430908203,
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2018-01-02T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 49.0,
+            'longitude': -108.0,
+            'time': '2018-01-02T06:00:00+00:00',
+            'u10': 3.4776244163513184,
+            'v10': 0.03294110298156738,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    def test_extract_rows__with_subset_variables(self):
+        actual = next(extract_rows(self.test_data_path, variables=['u10']))
+        expected = {
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2018-01-02T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 49.0,
+            'longitude': -108.0,
+            'time': '2018-01-02T06:00:00+00:00',
+            'u10': 3.4776244163513184,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    def test_extract_rows__specific_area(self):
+        actual = next(extract_rows(self.test_data_path, area=[45, -103, 33, -92]))
+        expected = {
+            'd2m': 246.19993591308594,
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2018-01-02T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 45.0,
+            'longitude': -103.0,
+            'time': '2018-01-02T06:00:00+00:00',
+            'u10': 2.73445987701416,
+            'v10': 0.08277571201324463,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    def test_extract_rows__specify_import_time(self):
+        now = datetime.datetime.utcnow().isoformat()
+        actual = next(extract_rows(self.test_data_path, import_time=now))
+        expected = {
+            'd2m': 242.3035430908203,
+            'data_import_time': now,
+            'data_first_step': '2018-01-02T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 49.0,
+            'longitude': -108.0,
+            'time': '2018-01-02T06:00:00+00:00',
+            'u10': 3.4776244163513184,
+            'v10': 0.03294110298156738
+        }
+        self.assertRowsEqual(actual, expected)
+
+    def test_extract_rows_single_point(self):
+        self.test_data_path = f'{self.test_data_folder}/test_data_single_point.nc'
+        actual = next(extract_rows(self.test_data_path))
+        expected = {
+            'd2m': 242.3035430908203,
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2018-01-02T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 49.0,
+            'longitude': -108.0,
+            'time': '2018-01-02T06:00:00+00:00',
+            'u10': 3.4776244163513184,
+            'v10': 0.03294110298156738,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    def test_extract_rows_nan(self):
+        self.test_data_path = f'{self.test_data_folder}/test_data_has_nan.nc'
+        actual = next(extract_rows(self.test_data_path))
+        expected = {
+            'd2m': 242.3035430908203,
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2018-01-02T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 49.0,
+            'longitude': -108.0,
+            'time': '2018-01-02T06:00:00+00:00',
+            'u10': None,
+            'v10': 0.03294110298156738,
+        }
+        self.assertRowsEqual(actual, expected)
+
+
+@contextmanager
+def timing(description: str) -> t.Iterator[None]:
+    logging.info(f"{description}: Starting profiler")
+    start = perf_counter()
+    yield
+    elapsed_time = perf_counter() - start
+    logging.info(f"{description}: Elapsed time: {elapsed_time:0.4f} seconds")
+
+
+class ExtractRowsGribSupportTest(ExtractRowsTestBase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.test_data_path = f'{self.test_data_folder}/test_data_grib_single_timestep'
+
+    @_handle_missing_grib_be
+    def test_extract_rows(self):
+        actual = next(extract_rows(self.test_data_path))
+        expected = {
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2021-10-18T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 90.0,
+            'longitude': -180.0,
+            'number': 0,
+            'step': 0.0,
+            'surface': 0.0,
+            'time': '2021-10-18T06:00:00+00:00',
+            'valid_time': '2021-10-18T06:00:00+00:00',
+            'z': 1.42578125,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    @_handle_missing_grib_be
+    def test_extract_rows__with_vars__excludes_non_index_coords(self):
+        actual = next(extract_rows(self.test_data_path, variables=['z']))
+        expected = {
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2021-10-18T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 90.0,
+            'longitude': -180.0,
+            'z': 1.42578125,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    @_handle_missing_grib_be
+    def test_extract_rows__with_vars__includes_coordinates_in_vars(self):
+        actual = next(extract_rows(self.test_data_path, variables=['z', 'step']))
+        expected = {
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_first_step': '2021-10-18T06:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'latitude': 90.0,
+            'longitude': -180.0,
+            'step': 0,
+            'z': 1.42578125,
+        }
+        self.assertRowsEqual(actual, expected)
+
+    @_handle_missing_grib_be
+    def test_multiple_editions(self):
+        self.test_data_path = f'{self.test_data_folder}/test_data_grib_multiple_edition_single_timestep.bz2'
+        actual = next(extract_rows(self.test_data_path))
+        expected = {
+            'cape': 0.0,
+            'cbh': None,
+            'cp': 0.0,
+            'crr': 0.0,
+            'd2m': 248.3846893310547,
+            'data_first_step': '2021-12-10T12:00:00+00:00',
+            'data_import_time': '1970-01-01T00:00:00+00:00',
+            'data_uri': self.test_data_path,
+            'depthBelowLandLayer': 0.0,
+            'dsrp': 0.0,
+            'fdir': 0.0,
+            'hcc': 0.0,
+            'hcct': None,
+            'hwbt0': 0.0,
+            'i10fg': 7.41250467300415,
+            'latitude': 90.0,
+            'longitude': -180.0,
+            'lsp': 1.1444091796875e-05,
+            'mcc': 0.0,
+            'msl': 99867.3125,
+            'number': 0,
+            'p3020': 20306.701171875,
+            'sd': 0.0,
+            'sf': 1.049041748046875e-05,
+            'sp': 99867.15625,
+            'step': 28800.0,
+            'stl1': 251.02520751953125,
+            'surface': 0.0,
+            'swvl1': -1.9539930654413618e-13,
+            't2m': 251.18968200683594,
+            'tcc': 0.9609375,
+            'tcrw': 0.0,
+            'tcw': 2.314192295074463,
+            'tcwv': 2.314192295074463,
+            'time': '2021-12-10T12:00:00+00:00',
+            'tp': 1.1444091796875e-05,
+            'tsr': 0.0,
+            'u10': -4.6668853759765625,
+            'u100': -7.6197662353515625,
+            'u200': -9.176498413085938,
+            'v10': -3.2414093017578125,
+            'v100': -4.1650390625,
+            'v200': -3.6647186279296875,
+            'valid_time': '2021-12-10T20:00:00+00:00'
+        }
+        self.assertRowsEqual(actual, expected)
+
+    @_handle_missing_grib_be
+    def test_timing_profile(self):
+        self.test_data_path = f'{self.test_data_folder}/test_data_grib_single_timestep'
+        counter = 0
+        i = extract_rows(self.test_data_path)
+        # Read once to avoid counting dataset open times, etc.
+        _ = next(i)
+        with timing('Loop'):
+            for v in i:
+                # Don't do everything, 10K coordinates is enough for testing.
+                if counter >= 10000:
+                    break
+                if counter % 1000 == 0:
+                    logging.info(f'Processed {counter // 1000}k coordinates...')
+                counter += 1
+        logging.info(f'Finished {counter}')
+
+
+if __name__ == '__main__':
+    unittest.main()
