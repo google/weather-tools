214a03da5ac757521ce90c02b8d904c7b71f93f4
Alex Merose <alxr@google.com>
2022-04-27 11:48:25 -0700

    Proper handling of singleton partition dimensions.
    The downloader's partition system should work when a value of a partition key is a singleton and not a list. Currently, this is broken.

--- weather_dl/download_pipeline/parsers.py
+++ weather_dl/download_pipeline/parsers.py
@@ -410,6 +410,11 @@ def process_config(file: t.IO) -> Config:
     # Ensure consistent lookup.
     config['parameters']['partition_keys'] = partition_keys
 
+    # Ensure the cartesian-cross can be taken on singleton values for the partition.
+    for key in partition_keys:
+        if isinstance(selection[key], str):
+            selection[key] = [selection[key]]
+
     return Config.from_dict(config)
 
 
