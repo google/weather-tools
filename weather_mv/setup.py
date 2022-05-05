454be4d2a4760662f57390b70761ea65b5e7be43
mahrsee1997 <rahul@infocusp.in>
2022-05-06 01:52:52 +0530

    added google-cloud-storage in setup.py

--- weather_mv/setup.py
+++ weather_mv/setup.py
@@ -32,6 +32,7 @@ base_requirements = [
     "pandas",
     "xarray",
     "google-cloud-bigquery",
+    "google-cloud-storage",
     "pyparsing==2.4.2",  # Fix http2lib auth breakage
     "cfgrib",
     "netcdf4",
