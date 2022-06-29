a449b343bbc5ee0cca6d3d0cc108c182e3c3e3c7
Alex Merose <alxr@google.com>
2022-06-29 15:29:43 -0700

    Updated module doc, which was stale.

--- weather_mv/loader_pipeline/pipeline.py
+++ weather_mv/loader_pipeline/pipeline.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Pipeline for reflecting lots of NetCDF objects into a BigQuery table."""
+"""Pipeline for loading weather data into analysis-ready mediums, like Google BigQuery."""
 
 import argparse
 import logging
