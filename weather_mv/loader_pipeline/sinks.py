c45b9a4bc95822b3f1d542e2bf8d343332ae50d3
mahrsee1997 <rahul@infocusp.in>
2022-11-22 15:44:35 +0530

    remove _make_grib_dataset_inmem method.

--- weather_mv/loader_pipeline/sinks.py
+++ weather_mv/loader_pipeline/sinks.py
@@ -60,18 +60,6 @@ class ToDataSink(abc.ABC, beam.PTransform):
         pass
 
 
-def _make_grib_dataset_inmem(grib_ds: xr.Dataset) -> xr.Dataset:
-    """Copies all the vars in-memory to reduce disk seeks every time a single row is processed.
-
-    This also removes the need to keep the backing temp source file around.
-    """
-    data_ds = grib_ds.copy(deep=True)
-    for v in grib_ds.variables:
-        if v not in data_ds.coords:
-            data_ds[v].variable.values = grib_ds[v].variable.values
-    return data_ds
-
-
 def _preprocess_tif(ds: xr.Dataset, filename: str, tif_metadata_for_datetime: str) -> xr.Dataset:
     """Transforms (y, x) coordinates into (lat, long) and adds bands data in data variables.
 
@@ -256,7 +244,6 @@ def open_dataset(uri: str,
                  tif_metadata_for_datetime: t.Optional[str] = None) -> t.Iterator[xr.Dataset]:
     """Open the dataset at 'uri' and return a xarray.Dataset."""
     try:
-        # By copying the file locally, xarray can open it much faster via an in-memory copy.
         with open_local(uri) as local_path:
             _, uri_extension = os.path.splitext(uri)
             xr_dataset: xr.Dataset = __open_dataset_file(local_path,
