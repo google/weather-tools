646ac1e93752e41800c5939330e5b6d9f58f4fdc
deepgabani8 <deep@infocusp.in>
2023-01-04 09:52:16 +0000

    Sending subprocess argument in tuple.

--- weather_mv/loader_pipeline/ee.py
+++ weather_mv/loader_pipeline/ee.py
@@ -463,7 +463,7 @@ class ConvertToAsset(beam.DoFn):
     def process(self, uri: str) -> t.Iterator[AssetData]:
         """Opens grib files and yields AssetData."""
         queue = Queue(maxsize=2)
-        process = Process(target=self.convert_to_asset, args=[queue, uri])
+        process = Process(target=self.convert_to_asset, args=(queue, uri))
         process.start()
         process.join()
 
