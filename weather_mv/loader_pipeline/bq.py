fa45144a7a8b040c09346f22936049cb31f5bb4b
dabhicusp <sahildabhi@infocusp.com>
2023-05-29 16:12:34 +0530

    Refactor the code for Polygon.

--- weather_mv/loader_pipeline/bq.py
+++ weather_mv/loader_pipeline/bq.py
@@ -292,15 +292,16 @@ def prepare_coordinates(
             yield uri, list(chunk)
 
 
-def fetch_geo_polygon(latitude: float, longitude: float, grid_resolution : float) -> Polygon:
-    lower_left = [latitude - grid_resolution, longitude - grid_resolution]
-    upper_left = [latitude - grid_resolution, longitude + grid_resolution]
-    upper_right = [latitude + grid_resolution, longitude + grid_resolution]
-    lower_right = [latitude + grid_resolution, longitude - grid_resolution]
+def fetch_geo_polygon(latitude: float, longitude: float, lat_grid_resolution: float, lon_grid_resolution: float) -> Polygon:
+    """Create a Polygon based on latitude and longitude and resolution."""
+    lower_left = [latitude - lat_grid_resolution, longitude - lon_grid_resolution]
+    upper_left = [latitude - lat_grid_resolution, longitude + lon_grid_resolution]
+    upper_right = [latitude + lat_grid_resolution, longitude + lon_grid_resolution]
+    lower_right = [latitude + lat_grid_resolution, longitude - lon_grid_resolution]
     lat_lon_bound = [lower_left, upper_left, upper_right, lower_right]
-    
+
     for i, _ in enumerate(lat_lon_bound):
-        if lat_lon_bound[i][1] >= 180: 
+        if lat_lon_bound[i][1] >= 180:
             lat_lon_bound[i][1] = lat_lon_bound[i][1] - 360
     return Polygon([
         (lat_lon_bound[0][0], lat_lon_bound[0][1]),
@@ -326,14 +327,15 @@ def extract_rows(uri: str,
 
     with open_dataset(uri, open_dataset_kwargs, disable_grib_schema_normalization,
                       tif_metadata_for_datetime) as ds:
-        
-        # Find the grid_resolution. In case of single point we can't find grid resolution.  
+
+        # Find the grid_resolution. In case of single point we can't find grid resolution.
         should_create_polygon = False
-        if len(ds['latitude']) > 1:
+        if len(ds['latitude']) > 1 and len(ds['longitude']) > 1:
             # consider that Grid is regular.
-            grid_resolution = (ds["latitude"][1].values - ds["latitude"][0].values)//2
+            lat_grid_resolution = (ds["latitude"][1].values - ds["latitude"][0].values)/2
+            lon_grid_resolution = (ds["longitude"][1].values - ds["longitude"][0].values)/2
             should_create_polygon = True
-        
+
         data_ds: xr.Dataset = _only_target_vars(ds, variables)
 
         first_ts_raw = data_ds.time[0].values if isinstance(data_ds.time.values, np.ndarray) else data_ds.time.values
@@ -360,9 +362,9 @@ def extract_rows(uri: str,
             row[DATA_URI_COLUMN] = uri
             row[DATA_FIRST_STEP] = first_time_step
             row[GEO_POINT_COLUMN] = fetch_geo_point(row['latitude'], row['longitude'])
-            row[GEO_POLYGON_COLUMN] = fetch_geo_polygon(row['latitude'], row['longitude'], grid_resolution) \
+            row[GEO_POLYGON_COLUMN] = fetch_geo_polygon(row['latitude'], row['longitude'], lat_grid_resolution, lon_grid_resolution) \
                                       if should_create_polygon else None
-            
+
             # 'row' ends up looking like:
             # {'latitude': 88.0, 'longitude': 2.0, 'time': '2015-01-01 06:00:00', 'd': -2.0187, 'cc': 0.007812,
             #  'z': 50049.8, 'data_import_time': '2020-12-05 00:12:02.424573 UTC', ...}
