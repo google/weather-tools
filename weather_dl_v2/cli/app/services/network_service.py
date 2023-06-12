e803628d15b4cf85d7f9a796fbc7a083e3a49a6e
Aniket Singh Rawat <aniket@infocusp.com>
2023-06-12 18:27:26 +0530

    Added network service.

---
+++ weather_dl_v2/cli/app/services/network_service.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+class NetworkService:
+    def parse_response(self, response: requests.Response):
+        #TODO: implement other http status codes.
+        if(response.status_code != 200):
+            print("Server error! Try again later.")
+
+        parsed = json.loads(response.text)
+        return json.dumps(parsed, indent=3)
+        
+    def get(self, uri, header, query=None, payload=None):
+        try:
+            x = requests.get(uri, params=query, headers=header, data=payload)
+            return self.parse_response(x)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        
+    def post(self, uri, header, query=None, payload=None, file=None):
+        try:
+            x = requests.post(uri, params=query, headers=header, data=payload, files=file)
+            return self.parse_response(x)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        
+    def put(self, uri, header, query=None, payload=None, file=None):
+        try:
+            x = requests.put(uri, params=query, headers=header, data=payload, files=file)
+
+            return self.parse_response(x)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        
+    def delete(self, uri, header, query=None):
+        try:
+            x =  requests.delete(uri, params=query, headers=header)
+            return self.parse_response(x)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+        
+network_service = NetworkService()
