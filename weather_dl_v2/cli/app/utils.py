ff38f7cbbebeb4f489ca0ba6b7a7f480429236ed
Aniket Singh Rawat <aniket@infocusp.com>
2023-09-13 12:17:55 +0000

    lint fix

--- weather_dl_v2/cli/app/utils.py
+++ weather_dl_v2/cli/app/utils.py
@@ -31,14 +31,18 @@ def as_table(response: str):
         # convert response to list if not a list.
         data = [data]
 
-    if len(data)==0:
+    if len(data) == 0:
         return ""
 
     header = data[0].keys()
     # if any column has lists, convert that to a string.
     rows = [
-        [",\n".join([f"{i} {ele}" for i, ele in enumerate(val)]) 
-        if isinstance(val, list) else val for val in x.values()]
+        [
+            ",\n".join([f"{i} {ele}" for i, ele in enumerate(val)])
+            if isinstance(val, list)
+            else val
+            for val in x.values()
+        ]
         for x in data
     ]
     rows.insert(0, list(header))
