4e052dc716605604842d772af9eae53349869074
Aniket Singh Rawat <aniket@infocusp.com>
2023-12-17 15:53:00 +0000

    cli version bump

--- weather_dl_v2/cli/setup.py
+++ weather_dl_v2/cli/setup.py
@@ -21,7 +21,7 @@ setup(
     name="weather-dl-v2",
     packages=["app", "app.subcommands", "app.services"],
     install_requires=requirements,
-    version="0.0.1",
+    version="0.0.2",
     author="aniket",
     description=(
         "This cli tools helps in interacting with weather dl v2 fast API server."
