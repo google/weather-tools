45b4e33825cfba47c93a039b2f64a16c65d81f09
Darshan Prajapati <darshan.prajapati@infocusp.com>
2024-01-22 12:11:27 +0000

    Lint Fix

--- xql/xql/__init__.py
+++ xql/xql/__init__.py
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .apply import main, parse_query, run_query
+from .apply import main, parse_query, run_query #noqa
