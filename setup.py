0a7d5333f398f692525019e8b04d262c6dea84de
dabhicusp <sahildabhi@infocusp.com>
2024-09-03 08:55:18 +0000

    Lint issue fixed.

--- setup.py
+++ setup.py
@@ -117,7 +117,8 @@ setup(
     ],
     python_requires='>=3.8, <3.10',
     install_requires=['apache-beam[gcp]==2.40.0', 'gcsfs==2022.11.0'],
-    dependency_links=['https://github.com/dabhicusp/cdsapi-beta-google-weather-tools.git@master#egg=cdsapi'],  # TODO([#474](https://github.com/google/weather-tools/issues/474)): Compatible cdsapi with weather-dl.
+    dependency_links=['https://github.com/dabhicusp/cdsapi-beta-google-weather-tools.git@master#egg=cdsapi'],  # TODO([
+        #474](https://github.com/google/weather-tools/issues/474)): Compatible cdsapi with weather-dl.
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     scripts=['weather_dl/weather-dl', 'weather_mv/weather-mv', 'weather_sp/weather-sp'],
