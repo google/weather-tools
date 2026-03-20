dac9d0dacb8b21a08c8378bf21b2ff9df811d74a
Darshan Prajapati <93967637+DarshanSP19@users.noreply.github.com>
2026-03-20 15:07:02 +0530

    Support for Multiple Months,Years (#529)

--- weather_dl_v2/license_deployment/config.py
+++ weather_dl_v2/license_deployment/config.py
@@ -16,6 +16,7 @@
 import calendar
 import copy
 import dataclasses
+import itertools
 import typing as t
 
 Values = t.Union[t.List["Values"], t.Dict[str, "Values"], bool, int, float, str]  # pytype: disable=not-supported-yet
@@ -90,33 +91,36 @@ def optimize_selection_partition(selection: t.Dict) -> t.Dict:
         del selection_["date_range"]
 
     if "day" in selection_.keys() and selection_["day"] == "all":
-        year, month = selection_["year"], selection_["month"]
+        years, months = selection_["year"], selection_["month"]
 
-        multiples_error = (
-            "Cannot use keyword 'all' on selections with multiple '{type}'s."
-        )
+        multiples_error = "When using day='all' in selection, '/' is not allowed in {type}."
 
-        if isinstance(year, list):
-            assert len(year) == 1, multiples_error.format(type="year")
-            year = year[0]
+        if isinstance(years, str):
+            years = [years]
 
-        if isinstance(month, list):
-            assert len(month) == 1, multiples_error.format(type="month")
-            month = month[0]
+        if isinstance(months, str):
+            months = [months]
 
-        if isinstance(year, str):
-            assert "/" not in year, multiples_error.format(type="year")
+        date_ranges = []
 
-        if isinstance(month, str):
-            assert "/" not in month, multiples_error.format(type="month")
+        # Generating dates for every year-month.
+        for year, month in itertools.product(years, months):
 
-        year, month = int(year), int(month)
+            if isinstance(year, str):
+                assert "/" not in year, multiples_error.format(type="year")
 
-        _, n_days_in_month = calendar.monthrange(year, month)
+            if isinstance(month, str):
+                assert "/" not in month, multiples_error.format(type="month")
 
-        selection_[
-            "date"
-        ] = f"{year:04d}-{month:02d}-01/to/{year:04d}-{month:02d}-{n_days_in_month:02d}"
+            year, month = int(year), int(month)
+
+            _, n_days_in_month = calendar.monthrange(year, month)
+
+            date_range = [f'{year:04d}-{month:02d}-{day:02d}' for day in range(1, n_days_in_month + 1)]
+
+            date_ranges.extend(date_range)
+
+        selection_["date"] = date_ranges
         del selection_["day"]
         del selection_["month"]
         del selection_["year"]
