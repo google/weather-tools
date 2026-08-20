042e091b2d849e3c82354d8bca1f0998448a1de1
kcshah <kcshah@google.com>
2026-08-20 07:47:19 +0000

    Code fix for no_cache param  in weather-dl-v2

--- weather_dl_v2/fastapi-server/config_processing/parsers.py
+++ weather_dl_v2/fastapi-server/config_processing/parsers.py
@@ -493,11 +493,11 @@ def process_config(file: t.IO, config_name: str) -> Config:
             require(no_cache in ["true", "false"],
                     """no_cache can only be true or false.""")
 
-    if selection['no_cache'] == "true":
-        # To download the fresh data and not from cache.
-        # https://forum.ecmwf.int/t/how-to-avoid-the-cds-cache-issue/905
-        config['selection']['nocache'] = '123'
-    del config['selection']['no_cache']
+        if selection['no_cache'] == "true":
+            # To download the fresh data and not from cache.
+            # https://forum.ecmwf.int/t/how-to-avoid-the-cds-cache-issue/905
+            config['selection']['nocache'] = '123'
+        del config['selection']['no_cache']
 
     # Ensure consistent lookup.
     config["parameters"]["partition_keys"] = partition_keys
