e0b02753c3008fa9276657c85caf9902aef0be1f
Shubham Kumar <shubham.kumar@infocusp.com>
2026-08-24 13:40:52 +0000

    adding os.path.join in test file

--- weather_sp/splitter_pipeline/file_splitters_test.py
+++ weather_sp/splitter_pipeline/file_splitters_test.py
@@ -192,12 +192,13 @@ class TestGribSplitter:
         assert splitter.should_skip()
 
     def test_splits_only_missing_files(self, data_dir, grib_splitter):
-        input_path = f'{data_dir}/era5_sample.grib'
-        split_dir = f'{data_dir}/split_files/'
+        input_path = os.path.join(data_dir, 'era5_sample.grib')
+        split_dir = os.path.join(data_dir, 'split_files')
+        out_file_prefix = os.path.join(split_dir, 'era5_sample')
         splitter = grib_splitter(
             input_path,
             OutFileInfo(
-                f'{data_dir}/split_files/era5_sample',
+                out_file_prefix,
                 formatting='_{typeOfLevel}_{shortName}',
                 ending='.grib',
                 template_folders=[])
@@ -211,11 +212,12 @@ class TestGribSplitter:
         }
 
         missing_file = 'era5_sample_isobaricInhPa_z.grib'
-        os.remove(os.path.join(split_dir, missing_file))
+        missing_file_path = os.path.join(split_dir, missing_file)
+        os.remove(missing_file_path)
 
         splitter.split_data()
 
-        assert os.path.exists(os.path.join(split_dir, missing_file))
+        assert os.path.exists(missing_file_path)
         for fname, orig_mtime in original_mtimes.items():
             if fname == missing_file:
                 continue
@@ -335,11 +337,12 @@ class TestNetCdfSplitter:
         assert splitter.should_skip()
 
     def test_splits_only_missing_files(self, data_dir):
-        input_path = f'{data_dir}/era5_sample.nc'
-        split_dir = f'{data_dir}/split_files/'
+        input_path = os.path.join(data_dir, 'era5_sample.nc')
+        split_dir = os.path.join(data_dir, 'split_files')
+        out_file_prefix = os.path.join(split_dir, 'era5_sample')
         splitter = NetCdfSplitter(
             input_path,
-            OutFileInfo(f'{data_dir}/split_files/era5_sample',
+            OutFileInfo(out_file_prefix,
                         formatting='_{time}_{variable}',
                         ending='.nc',
                         template_folders=[]))
@@ -352,11 +355,12 @@ class TestNetCdfSplitter:
         }
 
         missing_file = 'era5_sample_2015-01-15T00:00_z.nc'
-        os.remove(os.path.join(split_dir, missing_file))
+        missing_file_path = os.path.join(split_dir, missing_file)
+        os.remove(missing_file_path)
 
         splitter.split_data()
 
-        assert os.path.exists(os.path.join(split_dir, missing_file))
+        assert os.path.exists(missing_file_path)
         for fname, orig_mtime in original_mtimes.items():
             if fname == missing_file:
                 continue
